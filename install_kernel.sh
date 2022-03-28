577741c851c29b3247dedd6b172e5a888f50429b
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2022-03-29 01:25:42 +0800

    Create install_kernel.sh

---
+++ install_kernel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+KV=KVER
+DT=/boot/dtb/rockchip/
+echo "decompressing.."
+unzip Kernel.$KV.zip
+echo "copying dtb files.."
+cp *dtb $DT -v
+echo "installing kernel.."
+dpkg -i *deb
+echo "overriding current kernel boot image.."
+mv ./Image /boot/Image -v
+sync
+echo "done"
+#reboot
